581c9a1f360bc8d5f66b8ccc532642308ae12202
pk65arya <pk65arya@gmail.com>
2025-02-19 18:59:03 +0530

    first problem in 2d

---
+++ first2DProblem.js
@@ -0,0 +1,19 @@
+
+
+
+let N=3;
+let M=2;
+
+let arr=[
+  [1,2],[3,4],[5,6]
+];
+
+for(let i=0;i<N;i++){
+
+  let bag="";
+
+  for(let j=0;j<M;j++){
+    bag+=arr[i][j]+" ";
+  }
+  console.log(bag);
+}
