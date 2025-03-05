58e3ac399f83dda3c6d48fc4fa623a4b18c8b823
pk65arya <pk65arya@gmail.com>
2025-03-05 22:44:42 +0530

    L0 - Fixing Array and Object Operations

---
+++ fixingArray.js
@@ -0,0 +1,46 @@
+//L0 - Fixing Array and Object Operations
+
+const library = {
+
+  books: [{ title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937 }],
+  
+  addBook(book) {
+  
+  if (!book.title || !book.author || !book.year) {
+  
+  console.log("Book information is incomplete.");
+  
+  return;
+  
+  }
+  this.books.push(book);
+  
+  },
+  
+  findBookByTitle(title) {
+  
+  return this.books.find(book => book.title === title);
+  
+  },
+  
+  removeBook(title) {
+  
+  const index = this.books.findIndex(book => book.title === title);
+  
+  if (index !== -1) {
+  
+  this.books.splice(index, 1);
+  
+  } else {
+  
+  console.log("Book not found.");
+  
+  } } };
+  
+  library.addBook({ title: "The Hero",author: "George Orwell", year: 1949 });
+  
+  console.log(library.books.length);
+  
+  console.log(library.findBookByTitle("The Hero"))
+  
+  library.removeBook("The Hero");
