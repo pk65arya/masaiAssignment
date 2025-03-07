7647559e7e000f447fe505cd91b7c00a25d21f99
pk65arya <pk65arya@gmail.com>
2025-03-07 19:03:03 +0530

    L0 - Understanding Synchronous Code Execution

---
+++ syncBasic.js
@@ -0,0 +1,8 @@
+//L0 - Understanding Synchronous Code Execution
+
+//Write a JavaScript program that demonstrates synchronous code execution. You should create three console logs that print "Task 1", "Task 2", and "Task 3" in sequence. Since synchronous code runs line-by-line, the console output should display each task in the order they are written.
+
+
+console.log("Task 1");
+console.log("Task 2");
+console.log("Task 3");
