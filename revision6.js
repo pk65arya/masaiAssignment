673f9b1168bde5ef5c6ef0a8ab821c9706c7a10a
pk65arya <pk65arya@gmail.com>
2025-03-27 16:20:31 +0530

    L0 - Data Privacy using Closures

---
+++ revision6.js
@@ -0,0 +1,33 @@
+// L0 - Data Privacy using Closures
+
+// Problem Statement:
+// Create a closure that simulates a simple counter. Implement a function createCounter that has a private count variable. The function should return two methods: increment to increase the count by 1 and getCount to return the current value of the counter. Ensure that count cannot be accessed directly from outside the closure.
+
+// Example:
+
+// const counter = createCounter();
+
+// console.log(counter.increment()); // Output: 1;
+
+// console.log(counter.increment()); // Output: 2;
+
+// console.log(counter.getCount()); // Output: 2;
+
+function createCounter(){
+  let count=0;
+
+  return{
+    increment(){
+      count++;
+      return count;
+    },
+    getCount(){
+      return count;
+    }
+  }
+}
+
+let counter=createCounter();
+console.log(counter.increment());
+console.log(counter.increment());
+console.log(counter.getCount());
